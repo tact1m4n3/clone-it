f15eae739f02f3b0c4b04f872c1b42fe0c123746
tact1m4n3 <tudor.andrei.dicu@gmail.com>
2024-07-26 22:48:25 +0300

    initial commit

---
+++ build.zig
@@ -0,0 +1,45 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "galaxy-one-hackathon", // TODO: change this after we come up with a name
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
+
+    exe.addCSourceFiles(.{ .files = &[_][]const u8{
+        "src/stb/stb_truetype_impl.c",
+        "src/stb/stb_image_impl.c",
+    } });
+    exe.addIncludePath(b.path("src/stb"));
+
+    const glfw_dep = b.dependency("glfw", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.linkLibrary(glfw_dep.artifact("glfw"));
+    @import("glfw").addPaths(&exe.root_module);
+
+    const zgl_dep = b.dependency("zgl", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("zgl", zgl_dep.module("zgl"));
+
+    const zlm_dep = b.dependency("zlm", .{});
+    exe.root_module.addImport("zlm", zlm_dep.module("zlm"));
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+// const c = @import("c.zig");
+
+pub fn main() !void {
+    std.debug.print("Hello world\n", .{});
+}
